95830ac04a797d36da965c3627fe84226ea21077
Wahyu Wibowo <v-wahyu.wibowo@btpnsyariah.com>
2023-10-17 13:33:14 +0700

    minor config changes

--- src/main/java/com/example/NautbootApplication.java
+++ src/main/java/com/example/NautbootApplication.java
@@ -2,18 +2,16 @@ package com.example;
 
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 
 @OpenAPIDefinition(
         info = @Info(
-                title = "fifthone",
+                title = "nautboot",
                 version = "0.0",
                 description = "My API",
-                license = @License(name = "Apache 2.0", url = "https://foo.bar"),
-                contact = @Contact(url = "https://gigantic-server.com", name = "Fred", email = "Fred@gigagantic-server.com")
+                license = @License(name = "Apache 2.0", url = "https://foo.bar")
         )
 )
 public class NautbootApplication {
